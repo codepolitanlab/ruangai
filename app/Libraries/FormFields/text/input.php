9b8c1f20ba07875339a8f4dc769ff2465f5c78cd
Toni Haryanto <toha.samba@gmail.com>
2025-03-12 14:19:01 +0700

    Update core

--- app/Libraries/FormFields/text/input.php
+++ app/Libraries/FormFields/text/input.php
@@ -5,5 +5,6 @@
     value="<?= $value ?? ''; ?>" 
     placeholder="<?= $config['placeholder'] ?? '';?>" 
     class="form-control" 
-    data-caption="<?= $config['label'];?>" <?= strpos($config['rules'] ?? '', 'required') !== false ? 'required' : ''; ?> 
+    data-caption="<?= $config['label'];?>" 
+    <?= strpos($config['rules'] ?? '', 'required') !== false ? 'required' : ''; ?> 
     autocomplete="off"/>
