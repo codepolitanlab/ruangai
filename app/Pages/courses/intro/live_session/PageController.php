d30d1bc8aba96dcaea45ad254d4ffd404b8bf025
AldianBaim <aldiansyahibrahim5@gmail.com>
2025-04-23 10:16:42 +0700

    feat: loop data live session

--- app/Pages/courses/intro/live_session/PageController.php
+++ app/Pages/courses/intro/live_session/PageController.php
@@ -4,5 +4,31 @@ use App\Pages\BaseController;
 
 class PageController extends BaseController 
 {
-    
+    public $data = [
+        'page_title' => "Live Session"
+    ];
+
+    public function getData()
+    {
+        $db = \Config\Database::connect();
+        $live_sessions = $db->table('live_sessions')
+            ->get()
+            ->getResultArray();
+
+        if ($live_sessions) {
+
+            $data['live_sessions'] = $live_sessions;
+
+            return $this->respond([
+                'response_code'    => 200,
+                'response_message' => 'success',
+                'data'             => $data
+            ]);
+        } else {
+            return $this->respond([
+                'response_code'    => 404,
+                'response_message' => 'Not found',
+            ]);
+        }
+    }
 }
--- app/Pages/courses/intro/live_session/template.php
+++ app/Pages/courses/intro/live_session/template.php
@@ -1,4 +1,9 @@
-<div id="live_session">
+<div
+	id="live_session"
+	x-data="$heroic({
+        title: `<?= $page_title ?>`,
+        getUrl: `/courses/intro/live_session/data`
+    })">
 	<div id="app-header" class="appHeader main border-0">
 		<div class="left"><a class="headerButton" href="/courses"><i class="bi bi-chevron-left"></i></a></div>
 		<div class="pageTitle"><span>Detail Kelas</span></div>
@@ -83,10 +88,53 @@
 			</section>
 			<section>
 				<div class="container px-4">
+
+					<!-- Check if no live session -->
+					<template x-show="data.live_sessions.length == 0">
+						<div class="card card-hover rounded-20 shadow-none border p-3 mb-2">
+							<div class="row g-3">
+								<div class="col">
+									<div class="d-flex justify-content-between align-items-start">
+										<div>
+											<div class="d-flex gap-3 align-items-center">
+												<h5 class="text-pink m-0">Belum ada live session</h5>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</template>
 					<!-- Completed Live Sessions -->
-				<a href="/courses/intro/inggris-beginner-book-1/live_session/1">
-					<div class="card card-hover rounded-20 shadow-none border p-3 mb-2">
-						<div class="d-flex gap-3">
+					<template x-show="data.live_sessions.length > 0" x-for="live_session in data.live_sessions">
+						<a href="/courses/intro/inggris-beginner-book-1/live_session/1">
+							<div class="card card-hover rounded-20 shadow-none border p-3 mb-2">
+								<div class="row g-3">
+									<div class="col">
+										<div class="d-flex justify-content-between align-items-start">
+											<div>
+												<div class="d-flex gap-3 mb-1 align-items-center">
+													<h5 class="text-pink m-0" x-text="live_session.title"></h5>
+													<div class="badge btn-primary px-2">Rekaman</div>
+												</div>
+												<h4 class="mb-3" x-text="live_session.description"></h4>
+											</div>
+										</div>
+										<div class="d-flex gap-3">
+											<div><i class="bi bi-clock"></i> <span x-text="live_session.date"></span></div>
+											<div><i class="bi bi-people"></i> <span x-text="live_session.total_student"></span> Siswa</div>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</a>
+					</template>
+
+
+					<!-- Coming Soon Sessions -->
+					<!-- <div class="card rounded-20 p-3 mb-2 shadow-none border position-relative">
+						<div class="d-flex gap-3 position-relative" style="filter: blur(5px);opacity: 0.5;">
 							<div style="width: 200px; height: 130px">
 								<img src="https://plus.unsplash.com/premium_photo-1661766386981-1140b7b37193?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cHJvZmVzaW9uYWx8ZW58MHx8MHx8fDA%3D" class="rounded-3 cover w-100 object-fit-cover" height="100" alt="Live Session 1">
 							</div>
@@ -106,111 +154,22 @@
 								</div>
 							</div>
 						</div>
-					</div>
-				</a>
-
-				<a href="/courses/intro/inggris-beginner-book-1/live_session/1">
-					<div class="card card-hover rounded-20 shadow-none border p-3 mb-2">
-						<div class="d-flex gap-3">
-							<div style="width: 200px; height: 130px">
-								<img src="https://plus.unsplash.com/premium_photo-1661766386981-1140b7b37193?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cHJvZmVzaW9uYWx8ZW58MHx8MHx8fDA%3D" class="rounded-3 cover w-100 object-fit-cover" height="100" alt="Live Session 1">
-							</div>
-							<div class="flex-grow-1">
-								<div class="d-flex justify-content-between align-items-start">
-									<div>
-										<div class="d-flex gap-3 mb-1 align-items-center">
-											<h5 class="text-pink m-0">Live Session 1</h5>
-											<div class="badge btn-primary px-2">Rekaman</div>
-										</div>
-										<h4 class="mb-3">Mengenal Lebih Dalam Peran AI di Kehidupan Sehari-hari</h4>
+						<div class="position-absolute top-50 start-50 translate-middle rounded-3 p-2 w-100">
+							<div class="d-flex flex-column justify-content-center">
+								<h3 class="text-center mb-2">Akan dimulai pada</h3>
+								<div class="d-flex gap-4 justify-content-center">
+									<div class="d-flex align-items-center gap-2">
+										<i class="bi bi-calendar"></i>
+										<div>12 April 2025</div>
+									</div>
+									<div class="d-flex align-items-center gap-2">
+										<i class="bi bi-clock"></i>
+										<div>14:00</div>
 									</div>
 								</div>
-								<div class="d-flex gap-3">
-									<div><i class="bi bi-clock"></i> 50:22</div>
-									<div><i class="bi bi-people"></i> 120 Siswa</div>
-								</div>
 							</div>
 						</div>
-					</div>
-				</a>
-
-				<!-- Upcoming Live Session -->
-				<div class="card rounded-20 p-3 mb-2 shadow-none border position-relative">
-					<div class="d-flex gap-3 position-relative" style="filter: blur(5px);opacity: 0.5;">
-						<div style="width: 200px; height: 130px">
-							<img src="https://plus.unsplash.com/premium_photo-1661766386981-1140b7b37193?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cHJvZmVzaW9uYWx8ZW58MHx8MHx8fDA%3D" class="rounded-3 cover w-100 object-fit-cover" height="100" alt="Live Session 1">
-						</div>
-						<div class="flex-grow-1">
-							<div class="d-flex justify-content-between align-items-start">
-								<div>
-									<div class="d-flex gap-3 mb-1 align-items-center">
-										<h5 class="text-pink m-0">Live Session 1</h5>
-										<div class="badge btn-primary px-2">Rekaman</div>
-									</div>
-									<h4 class="mb-3">Mengenal Lebih Dalam Peran AI di Kehidupan Sehari-hari</h4>
-								</div>
-							</div>
-							<div class="d-flex gap-3">
-								<div><i class="bi bi-clock"></i> 50:22</div>
-								<div><i class="bi bi-people"></i> 120 Siswa</div>
-							</div>
-						</div>
-					</div>
-					<div class="position-absolute top-50 start-50 translate-middle rounded-3 p-2 w-100">
-						<div class="d-flex flex-column justify-content-center">
-							<h3 class="text-center mb-2">Akan dimulai pada</h3>
-							<div class="d-flex gap-4 justify-content-center">
-								<div class="d-flex align-items-center gap-2">
-									<i class="bi bi-calendar"></i>
-									<div>12 April 2025</div>
-								</div>
-								<div class="d-flex align-items-center gap-2">
-									<i class="bi bi-clock"></i>
-									<div>14:00</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<!-- Coming Soon Sessions -->
-				<div class="card rounded-20 p-3 mb-2 shadow-none border position-relative">
-					<div class="d-flex gap-3 position-relative" style="filter: blur(5px);opacity: 0.5;">
-						<div style="width: 200px; height: 130px">
-							<img src="https://plus.unsplash.com/premium_photo-1661766386981-1140b7b37193?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cHJvZmVzaW9uYWx8ZW58MHx8MHx8fDA%3D" class="rounded-3 cover w-100 object-fit-cover" height="100" alt="Live Session 1">
-						</div>
-						<div class="flex-grow-1">
-							<div class="d-flex justify-content-between align-items-start">
-								<div>
-									<div class="d-flex gap-3 mb-1 align-items-center">
-										<h5 class="text-pink m-0">Live Session 1</h5>
-										<div class="badge btn-primary px-2">Rekaman</div>
-									</div>
-									<h4 class="mb-3">Mengenal Lebih Dalam Peran AI di Kehidupan Sehari-hari</h4>
-								</div>
-							</div>
-							<div class="d-flex gap-3">
-								<div><i class="bi bi-clock"></i> 50:22</div>
-								<div><i class="bi bi-people"></i> 120 Siswa</div>
-							</div>
-						</div>
-					</div>
-					<div class="position-absolute top-50 start-50 translate-middle rounded-3 p-2 w-100">
-						<div class="d-flex flex-column justify-content-center">
-							<h3 class="text-center mb-2">Akan dimulai pada</h3>
-							<div class="d-flex gap-4 justify-content-center">
-								<div class="d-flex align-items-center gap-2">
-									<i class="bi bi-calendar"></i>
-									<div>12 April 2025</div>
-								</div>
-								<div class="d-flex align-items-center gap-2">
-									<i class="bi bi-clock"></i>
-									<div>14:00</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+					</div> -->
 				</div>
 			</section>
 		</div>
