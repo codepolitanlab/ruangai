6ca3b6d394474d5240a9e380dc7263ec331bb662
AldianBaim <aldiansyahibrahim5@gmail.com>
2025-05-14 14:47:58 +0700

    fix: change field birthday

--- app/Models/ScholarshipParticipantModel.php
+++ app/Models/ScholarshipParticipantModel.php
@@ -8,7 +8,7 @@ class ScholarshipParticipantModel extends Model
 {
     protected $table = 'scholarship_participants';
     protected $allowedFields = [
-        'user_id', 'fullname', 'email', 'whatsapp', 'date_of_birth',
+        'user_id', 'fullname', 'email', 'whatsapp', 'birthday',
         'gender', 'province', 'city', 'occupation', 'work_experience',
         'skill', 'institution', 'major', 'semester', 'grade',
         'type_of_business', 'business_duration', 'education_level', 'graduation_year', 'link_business', 'last_project', 'reference', 'program', 'referral_code', 'accept_terms', 'accept_agreement', 'withdrawal', 'status'
