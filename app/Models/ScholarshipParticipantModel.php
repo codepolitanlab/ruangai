5f4df0179eada3ca5898f414ecf5dd4e5d0ec611
AldianBaim <aldiansyahibrahim5@gmail.com>
2025-05-14 14:41:36 +0700

    fix: add accept tnc

--- app/Models/ScholarshipParticipantModel.php
+++ app/Models/ScholarshipParticipantModel.php
@@ -11,7 +11,7 @@ class ScholarshipParticipantModel extends Model
         'user_id', 'fullname', 'email', 'whatsapp', 'date_of_birth',
         'gender', 'province', 'city', 'occupation', 'work_experience',
         'skill', 'institution', 'major', 'semester', 'grade',
-        'type_of_business', 'business_duration', 'education_level', 'graduation_year', 'link_business', 'last_project', 'reference', 'program', 'referral_code'
+        'type_of_business', 'business_duration', 'education_level', 'graduation_year', 'link_business', 'last_project', 'reference', 'program', 'referral_code', 'accept_terms', 'accept_agreement', 'withdrawal', 'status'
     ];
     protected $useTimestamps = true;
 }
