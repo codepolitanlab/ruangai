30d31fd68d0de2fe4fa875f5799e503cac679393
Toni Haryanto <toha.samba@gmail.com>
2025-05-28 15:34:30 +0700

    Add setting to enable live recording

--- app/Config/Course.php
+++ app/Config/Course.php
@@ -8,4 +8,6 @@ class Course extends BaseConfig
 {
     // Walktu untuk menunggu sampai tombol "Saya sudah paham" aktif (dalam milidetik)
     public $waitToEnableButtonUnderstand = 1000 * 60;
+
+    public $enableLiveRecording = false;
 }
