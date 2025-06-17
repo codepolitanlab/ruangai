7944f3f9ced6dc66d7f7cf073612a541fd6c57d9
AldianBaim <aldiansyahibrahim5@gmail.com>
2025-06-17 15:42:08 +0700

    feat: add new field expire_at on course_students

---
+++ app/Database/Migrations/2025-06-17-074519_AddColumnExpireAtToCourseStudents.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddColumnExpireAtToCourseStudents extends Migration
+{
+    public function up()
+    {
+        $this->forge->addColumn('course_students', [
+            'expire_at' => [
+                'type' => 'timestamp',
+                'null' => true,
+                'after' => 'graduate',
+            ]
+        ]);
+    }
+
+    public function down()
+    {
+        $this->forge->dropColumn('course_students', ['expire_at']);
+    }
+}
