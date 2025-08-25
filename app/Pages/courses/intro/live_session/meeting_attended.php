70f3af1bd35fc4c77cb86d7eb3b9f92ae7c5c561
Toni Haryanto <toha.samba@gmail.com>
2025-08-25 20:10:04 +0700

    Fix warning in live session for indicating on process

--- app/Pages/courses/intro/live_session/meeting_attended.php
+++ app/Pages/courses/intro/live_session/meeting_attended.php
@@ -6,8 +6,8 @@
         <div class="d-flex gap-2 align-items-top">
             <div class="py-2 ps-1 pe-2">
                 <div class="d-flex align-items-center justify-content-center rounded-4 bg-white">
-                    <i class="bi bi-check fs-2"
-                        :class="attended.status == 1 ? 'bi-check text-success' : 'bi-x text-danger'"></i>
+                    <i class="bi fs-2"
+                        :class="{'bi-check text-success': attended.status == 1, 'bi-x text-danger': attended.status == 0, 'bi-arrow-repeat text-warning': attended.status == null}"></i>
                 </div>
             </div>
             <div class="py-2">
@@ -27,8 +27,14 @@
                     <p 
                         class="bg-white bg-opacity-50 py-1 px-2 rounded-4 mt-3 mb-0"
                         x-show="attended.duration < 1">
-                        <i class="bi bi-exclamation-triangle"></i>
-                        Kamu tidak mengikuti sesi
+                        <span x-show="attended.status == null">
+                            <i class="bi bi-arrow-repeat"></i>
+                            Data kehadiran sedang diproses
+                        </span>
+                        <span x-show="attended.status == 0">
+                            <i class="bi bi-exclamation-triangle"></i>
+                            Kamu tidak mengikuti sesi
+                        </span>
                     </p>
                     <p 
                         class="bg-white bg-opacity-50 py-1 px-2 rounded-4 mt-3 mb-0"
