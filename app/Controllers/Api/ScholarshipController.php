4ab20043c994d85b93d3a5cf7a7e45e33f910100
AldianBaim <aldiansyahibrahim5@gmail.com>
2025-08-26 14:25:40 +0700

    debug

--- app/Controllers/Api/ScholarshipController.php
+++ app/Controllers/Api/ScholarshipController.php
@@ -196,9 +196,8 @@ class ScholarshipController extends ResourceController
             ];
         }
 
-        $memberQuery = $participantModel->select('fullname, status, scholarship_participants.created_at as joined_at, course_students.graduate, course_students.progress, COUNT(live_attendance.id) as total_live_session')
+        $memberQuery = $participantModel->select('fullname, status, scholarship_participants.created_at as joined_at, course_students.graduate, course_students.progress')
             ->join('course_students', 'course_students.user_id = scholarship_participants.user_id', 'left')
-            ->join('live_attendance', 'live_attendance.user_id = scholarship_participants.user_id', 'left')
             ->where('scholarship_participants.reference', $leader['referral_code'])
             ->where('scholarship_participants.deleted_at', null)
             ->get();
