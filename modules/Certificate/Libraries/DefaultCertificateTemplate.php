7ecd996b6b6a3ae49f0eeb90f97a4218cc0e377e
AldianBaim <aldiansyahibrahim5@gmail.com>
2025-11-07 13:01:01 +0700

    fix: double certificate bug

--- modules/Certificate/Libraries/DefaultCertificateTemplate.php
+++ modules/Certificate/Libraries/DefaultCertificateTemplate.php
@@ -71,50 +71,50 @@ class DefaultCertificateTemplate extends CertificateTemplate
                     ]
                 ),
                 // Buat halaman 2 bahasa inggris
-                $this->createPage(
-                    base_url('certificates/tpl/en_1.tpl-min.jpg'),
-                    [
-                        'name' => $this->createPosition(
-                            xPct: 4.6,
-                            yPct: 45,
-                            maxWidthPct: 70,
-                            fontMm: 12,
-                            minFontMm: 5.5,
-                            weight: 'bold',
-                            align: 'left',
-                            color: '#174658',
-                            autoshrink: true
-                        ),
-                        'publishDate' => $this->createPosition(
-                            xPct: 4.6,
-                            yPct: 75,
-                            maxWidthPct: 30,
-                            fontMm: 5,
-                            minFontMm: 3.0,
-                            weight: 'normal',
-                            align: 'left'
-                        ),
-                        'code' => $this->createPosition(
-                            xPct: 95,
-                            yPct: 86,
-                            maxWidthPct: 30,
-                            fontMm: 5,
-                            minFontMm: 3.5,
-                            weight: 'normal',
-                            align: 'right'
-                        ),
-                        'expiredDate' => $this->createPosition(
-                            xPct: 95,
-                            yPct: 90,
-                            maxWidthPct: 30,
-                            fontMm: 5,
-                            minFontMm: 3.0,
-                            weight: 'normal',
-                            align: 'right',
-                            prefix: 'Valid until '
-                        ),
-                    ]
-                ),
+                // $this->createPage(
+                //     base_url('certificates/tpl/en_1.tpl-min.jpg'),
+                //     [
+                //         'name' => $this->createPosition(
+                //             xPct: 4.6,
+                //             yPct: 45,
+                //             maxWidthPct: 70,
+                //             fontMm: 12,
+                //             minFontMm: 5.5,
+                //             weight: 'bold',
+                //             align: 'left',
+                //             color: '#174658',
+                //             autoshrink: true
+                //         ),
+                //         'publishDate' => $this->createPosition(
+                //             xPct: 4.6,
+                //             yPct: 75,
+                //             maxWidthPct: 30,
+                //             fontMm: 5,
+                //             minFontMm: 3.0,
+                //             weight: 'normal',
+                //             align: 'left'
+                //         ),
+                //         'code' => $this->createPosition(
+                //             xPct: 95,
+                //             yPct: 86,
+                //             maxWidthPct: 30,
+                //             fontMm: 5,
+                //             minFontMm: 3.5,
+                //             weight: 'normal',
+                //             align: 'right'
+                //         ),
+                //         'expiredDate' => $this->createPosition(
+                //             xPct: 95,
+                //             yPct: 90,
+                //             maxWidthPct: 30,
+                //             fontMm: 5,
+                //             minFontMm: 3.0,
+                //             weight: 'normal',
+                //             align: 'right',
+                //             prefix: 'Valid until '
+                //         ),
+                //     ]
+                // ),
                 // Buat halaman 3 - rekap
                 $this->createPage(
                     base_url('certificates/tpl/back_1.tpl-min.jpg'),
