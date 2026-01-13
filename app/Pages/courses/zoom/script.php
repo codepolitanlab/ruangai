e2b9402ff5bdff7e2d74a2ad904e32a98194a6f6
AldianBaim <aldiansyahibrahim5@gmail.com>
2026-01-13 11:36:56 +0700

    fix: add mentee co-mentor check for program access validation

--- app/Pages/courses/zoom/script.php
+++ app/Pages/courses/zoom/script.php
@@ -22,12 +22,12 @@
                     //     alert(`Maaf, kamu tidak dapat akses halaman live session. Silakan ikuti live session dari Co-Mentor ${value.comentor}.`);
                     //     window.location.replace(`${base_url}/courses/intro/1/dasar-dan-penggunaan-generative-ai/`);
                     // }
-                    if (value?.program != Alpine.store('core').activeProgram && !value?.is_comentor && !value?.is_mentor && !(value?.is_reference_followup && value?.student.graduate == '0') && !value?.is_followup) {
+                    if (value?.program != Alpine.store('core').activeProgram && !value?.is_comentor && !value?.is_mentor && !(value?.is_reference_followup && value?.student.graduate == '0') && !value?.is_followup && !value?.is_mentee_comentor) {
                         alert('Maaf, kamu belum terdaftar sebagai peserta program RuangAI Chapter ini. Silakan daftar ulang ya!');
-                        // window.location.replace(`${base_url}`);
+                        window.location.replace(`${base_url}`);
                     } else if(value?.is_participating_other_ai_program && value?.student.graduate == 1) {
                         alert('Anda sudah dinyatakan lulus program ini. Anda tetap dapat mengakses materi pembelajaran lainnya di RuangAI.');
-                        // window.location.replace(`${base_url}`);
+                        window.location.replace(`${base_url}`);
                     }
                 });
             },
