fb4b12eb7c6d6e437a58fd2aa789b4d632bd2590
Aldiansyah Ibrahim <aldiansyahibrahim5@gmail.com>
2026-04-02 13:46:16 +0700

    feat: update total graduated calculation to use database query

--- app/Controllers/Api/ScholarshipController.php
+++ app/Controllers/Api/ScholarshipController.php
@@ -420,7 +420,7 @@ class ScholarshipController extends ResourceController
                 'RuangAI2025B3' => $graduatedB3 ?? 0,
                 'RuangAI2025B4' => $graduatedB4 ?? 0,
             ];
-            $data['total_graduated']    = $graduatedB1 + $graduatedB2 + $graduatedB3 + $graduatedB4;
+            $data['total_graduated'] = $db->table('view_participants')->where('graduate', 1)->countAllResults();
         }
 
         return $this->respond($data);
