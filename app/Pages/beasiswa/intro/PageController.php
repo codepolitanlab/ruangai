163f5de6f0d9ab5effbfaab14beb9071f7b155e5
AldianBaim <aldiansyahibrahim5@gmail.com>
2026-04-23 11:17:01 +0700

    fix: correct reference casing for Mentor-Felisha in live session query

--- app/Pages/beasiswa/intro/PageController.php
+++ app/Pages/beasiswa/intro/PageController.php
@@ -141,7 +141,7 @@ class PageController extends BaseController
 
             $today = date('Y-m-d');
             $now   = date('H:i:s');
-            $isMentorFelisha = isset($this->data['student']['reference']) && $this->data['student']['reference'] === 'Mentor-Felisha';
+            $isMentorFelisha = isset($this->data['student']['reference']) && $this->data['student']['reference'] === 'mentor-felisha';
             $nextLiveSessionQuery = $db->table('live_meetings')
                 ->select('live_meetings.*, live_batch.name as batch_name, live_meetings.zoom_link, live_meetings.zoom_meeting_id, live_meetings.meeting_duration')
                 ->join('live_batch', 'live_batch.id = live_meetings.live_batch_id')
