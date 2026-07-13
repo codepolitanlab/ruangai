7012ecc1a14f5e9690d07711466c4ca5ecfd24e8
Aldiansyah Ibrahim <aldiansyahibrahim5@gmail.com>
2026-07-13 15:08:05 +0700

    fix: update access check for live session registration

--- app/Pages/beasiswa/intro/live_session/script.php
+++ app/Pages/beasiswa/intro/live_session/script.php
@@ -26,7 +26,7 @@
                     const base_url = window.location.origin;
                     
                     // Cek apakah user memiliki akses ke live session
-                    if (value?.program != Alpine.store('core').activeProgram && !value?.is_comentor && !value?.is_mentor && !value?.is_reference_followup && !value?.is_followup && !value?.is_mentee_comentor) {
+                    if (!value?.is_comentor && !value?.is_mentor && !value?.is_reference_followup && !value?.is_followup && !value?.is_mentee_comentor) {
                         alert('Maaf, kamu belum terdaftar sebagai peserta program RuangAI Chapter ini. Silakan daftar ulang ya!');
                         window.location.replace(`${base_url}`);
                     } else if(value?.is_participating_other_ai_program && value?.student?.graduate == 1) {
